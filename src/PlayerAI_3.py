77035ffa22d6388f498a249154fade788f68cc12
salewtg <salewtg@yahoo.com>
2018-03-12 23:56:29 +0100

    functions  to generate all possible moves

--- src/PlayerAI_3.py
+++ src/PlayerAI_3.py
@@ -10,4 +10,31 @@ class PlayerAI(BaseAI):
     def getMove(self, grid):
 
         moves = grid.getAvailableMoves()
+        self.generateMoves(grid)
+        testGrid = grid
+        compMoves = self.generateComputerMoves(testGrid)
         return moves[randint(0, len(moves) - 1)] if moves else None
+
+
+    def generateMoves(self,grid):
+        result = []
+        moves = grid.getAvailableMoves()
+        for m in moves:
+            newGrid = grid
+            newGrid.move(m)
+            result.append(newGrid)
+        return result
+
+    def generateComputerMoves(self,grid):
+        result = []
+        moves = grid.getAvailableCells()
+        for m in moves:
+            newGrid1 = grid
+            newGrid2 = grid
+            newGrid1.insertTile(m, 2)
+            newGrid2.insertTile(m, 4)
+            result.append(newGrid1)
+            result.append(newGrid2)
+
+        print("computer's moves available:", len(result))
+        return result
